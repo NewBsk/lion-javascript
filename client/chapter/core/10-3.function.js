c89908f791bf547d49d986813b646f7bbc48d681
NewBsk <pwo1101@hanmail.net>
2023-07-12 22:35:26 +0900

    ~0712 prototype 수업내용

--- client/chapter/core/10-3.function.js
+++ client/chapter/core/10-3.function.js
@@ -39,19 +39,40 @@ const result = calcAllMoney(1000, 500, 200, 2000);
 
 // 화살표 함수와 this
 
-// 함수 선언문
-function normalFunction() {}
+// 함수선언문
+function normalFunction() {
+  //  console.log(this);
+}
+
+window.normalFunction();
 
 // 함수표현식
-const expressionFunction = function () {};
+const expressionFunction = function () {
+  //  console.log(this);
+};
 
-// 화살표함수
-const arrowFunction = () => {};
+// 화살표함수식
+const arrowFunction = () => {
+  //  console.log(this);
+};
 
-/* 다음 함수를 작성해봅니다. -------------------------------------------------- */
+arrowFunction();
 
-// pow(numeric: number, powerCount: number): number;
-let pow;
+// 객체 안에서 this
 
-// repeat(text: string, repeatCount: number): string;
-let repeat;
+// 객체의 메서드를 정의할때는 화살표 함수보다 일반 함수가 더 좋은거 아닌가요? 맞습니다.
+// 메서드 안에서 함수를 호출할때는 화살표 함수가 더 좋아요
+
+//일반함수 : 나를 호출한 대상을 'this로' 바인딩한다.
+//화살표함수 : this를 바인딩 하지 않음. 부모요소의 값을 가지고 온다.
+
+const user = {
+  total: 0,
+  name: 'tiger',
+  age: 32,
+  address: '서울시 중랑구 면목동',
+  grades: [80, 90, 100],
+  totalGrades: function () {
+    console.log(this);
+  },
+};
