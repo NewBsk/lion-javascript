c61d39a1518c4ed6d8d83f29161e6c756912fbbe
NewBsk <pwo1101@hanmail.net>
2023-07-27 15:45:25 +0900

    ~ localStorage

--- client/main.js
+++ client/main.js
@@ -1,1 +1,18 @@
-console.log('hello js!');
+import { getNode, setStorage, getStorage } from './lib/index.js';
+
+const textField = getNode('#textField');
+
+function handleTextField() {
+  const value = this.value;
+
+  setStorage('text', value);
+}
+
+function init() {
+  getStorage('text').then((res) => {
+    textField.value = res;
+  });
+}
+
+textField.addEventListener('input', handleTextField);
+window.addEventListener('DOMContentLoaded', init);
