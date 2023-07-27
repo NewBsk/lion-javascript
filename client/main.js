961bce8605ee337621bd950c0d4d882d37a08eed
NewBsk <pwo1101@hanmail.net>
2023-07-27 09:15:47 +0900

    ~fetch

--- client/main.js
+++ client/main.js
@@ -1,5 +1,19 @@
-import { tiger } from './lib/index.js';
+import { insertLast, tiger } from './lib/index.js';
 
-const data = tiger.get('https://jsonplaceholder.typicode.com/users');
+const URL = 'https://jsonplaceholder.typicode.com/users/1';
 
-console.log(data);
+//const url = tiger.get('https://jsonplaceholder.typicode.com/users');
+
+// const response = await fetch(URL);
+// const data = await response.json();
+
+// console.log( data );
+
+// fetch(URL).then((result)=>{
+
+//     result // response object
+//     return result.json()
+// })
+// .then((result)=>{
+//   console.log( result );
+// })
